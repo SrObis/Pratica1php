c1d01145acea9752b65c957d0b155a19195f2561
SrObis <obis2001@gmail.com>
2021-01-12 09:38:56 +0100

    Creacion de base de datos

--- bd/ventas.sql
+++ bd/ventas.sql
@@ -20,7 +20,9 @@ create table categorias (
 				primary key(id_categoria)
 				);
 
-create table imagenes(--cada producto tiene una imagen por loque se necesitara una tabla 2:52
+create table imagenes(--cada producto tiene una imagen por loque se necesitara una tabla
+					  --contendra un id_imagen como la ruta, esto nos aydara a relacionar con los productos
+					  --
 				id_imagen int auto_increment,
 				id_categoria int not null,
 				nombre varchar(500),
@@ -31,8 +33,8 @@ create table imagenes(--cada producto tiene una imagen por loque se necesitara u
 create table articulos(
 				id_producto int auto_increment,
 				id_categoria int not null,
-				id_imagen int not null,
-				id_usuario int not null,
+				id_imagen int not null, --id_imagen para mostrar la imagen de la tabla anterior
+				id_usuario int not null, --para tener mayor control
 				nombre varchar(50),
 				descripcion varchar(500),
 				cantidad int,
@@ -40,11 +42,11 @@ create table articulos(
 				fechaCaptura date,
 				primary key(id_producto)
 
-						);
+				);
 
 create table clientes(
 				id_cliente int auto_increment,
-				id_usuario int not null,
+				id_usuario int not null,--para saber que usuario ha creado el nuevo cliente
 				nombre varchar(200),
 				apellido varchar(200),
 				direccion varchar(200),
@@ -52,13 +54,12 @@ create table clientes(
 				telefono varchar(200),
 				rfc varchar(200),
 				primary key(id_cliente)
-					);
--- Recuerda agregar el id de usuario por favor 
+				);
 create table ventas(
 				id_venta int not null,
 				id_cliente int,
-				id_producto int,
+				id_producto int,--quando agas una venta se quitara la cantidad del almacen(articulos,cantidad int)
 				id_usuario int,
 				precio float,
 				fechaCompra date
-					);
+				);
