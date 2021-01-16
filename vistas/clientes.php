b76b833c3c5a59582fe71dc0bcd62fb15d10e3a6
Jordi Obis Farre <65295720+SrObis@users.noreply.github.com>
2021-01-16 15:39:07 +0100

    Maquetacion de modulo clientes 13

--- vistas/clientes.php
+++ vistas/clientes.php
@@ -14,11 +14,67 @@
 
 </head>
 <body>
+        <div class="container">
+            <h1>Clientes</h1>
+            <div class="row">
+                <div class="col-sm-4">
+                   <form id="frmClientes">
+                        <label>Nombre</label>
+                        <input type="text" class="form-control input-sm" id="nombre" name="nombre">
+                        <label>Apellido</label>
+                        <input type="text" class="form-control input-sm" id="apellido" name="apellido">
+                        <label>Direccion</label>
+                        <input type="text" class="form-control input-sm" id="direccion" name="direccion">
+                        <label>Email</label>
+                        <input type="text" class="form-control input-sm" id="email" name="email">
+                        <label>Telefono</label>
+                        <input type="text" class="form-control input-sm" id="telefono" name="telefono">
+                        <label>DNI</label>
+                        <input type="text" class="form-control input-sm" id="dni" name="dni">
+                        <p></p>
+                        <span class="btn btn-primary" id="btnAfregarCliente">Agregar</span>
 
+                   </form>
+                </div>
+                <div class="col-sm-8">
+                    <div id="tablaClientesLoad"></div>
+                </div>
+
+            </div>
+        </div>
 
 </body>
 </html>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('tablaClientesLoad').load("clientes/tablaClientes.php");
+       //script para evento click y ajax 
+        $('#btnAgregaCliente').click(function(){
+             //quanado de click que vaya a la funcion validar
+             vacios=validarFormVacio('frmClientes');
+                if(vacios>0){//no puede haber ningun campo bacio
+                    alertify.alert("Todos los campos tienes que estar llenos, no puede haber ninguno vacio");
+                    return false;
+                }
 
+            datos=$('#frmClientes').serialize();
+            $.ajax({
+                type:"POST",
+                data:datos,
+                url:"../procesos/clientes/agregarCliente.php",
+                success:function(r){
+                    if(r==1){
+                        $('tablaClientesLoad').load("clientes/tablaClientes.php");//cundo se inserte algo deve de cargar automaticamente
+
+                        alertify.success("Categoria agregada con exito");
+                    }else{
+                        alertify.error("No se pudo agregar la categoria");
+                    }
+                }
+            });
+        }); 
+    });
+</script>
 <?php 
 
     }else{//si no detecta secion que nos vaya a inicio
---
+++ vistas/clientes/tablaClientes.php
@@ -0,0 +1,37 @@
+<div class="table-responsive"> <!-- Quando la tabla este muy grande y el dispositivo sea pequeño saldra un scrol -->
+
+    <table class="table table-hover table-condensed table-bordered" style="text-align: center;">
+        <caption><label>Clientes</label></caption>
+        <tr>
+            <td>Nombre</td>
+            <td>Apellido</td>
+            <td>Direccion</td>
+            <td>Email</td>
+            <td>Telefono</td>
+            <td>DNI</td>
+            <td>Editar</td>
+            <td>Eliminar</td>
+
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+            <span class="btn btn-warning btn-xm">
+                <span class="glyphicon glyphicon-pencil"></span>
+            </span>
+            </td>
+            <td>
+                <span class="btn btn-danger btn-xm">
+                    <span class="glyphicon glyphicon-remove">
+                </span></span>
+            </td>
+            
+        </tr>
+    </table>
+
+</div>
